dfa815dddffb9f107aad5e5080b4a89cf66c0c38
septumfunk <jfleischman6@gmail.com>
2026-04-27 01:31:47 -0700

    Update bytecode.c

--- src/bytecode.c
+++ src/bytecode.c
@@ -302,6 +302,7 @@ char *solu_realpath(const char *base_file, const char *path) {
 }
 #elif defined(__vita__) && defined(VITA_DATA)
 char *solu_realpath(const char *path) {
+    if (strchr(path, ':')) return strdup(path);
     return sf_str_fmt("%s/%s", VITA_DATA, path).c_str;
 }
 #elif defined(POSIX_COMPAT)
